c3beae2fad31986768f9ef764a349ce954f57432
itookyourboo <wignorbo@yandex.ru>
2022-08-05 19:30:25 +0300

    sql bnf

---
+++ grammar_loader.py
@@ -0,0 +1,17 @@
+from lark import Lark
+
+
+def _get_grammar() -> str:
+    with open('sql.bnf') as sql_bnf_file:
+        return sql_bnf_file.read()
+
+
+__parser = None
+
+
+def get_parser() -> Lark:
+    global __parser
+    if not __parser:
+        grammar: str = _get_grammar()
+        __parser: Lark = Lark(grammar)
+    return __parser
