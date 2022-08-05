5d72090215ae72779f91214d636efdae74a3c268
Pavel Solomatin <crave_ozer_man@mail.ru>
2022-08-05 22:03:42 +0300

    ci: создать CLI v0

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    pass
+import argparse
+import sys
+import typing as tp
+
+
+def main(*files: str):
+    for file in files:
+        with open(file, "r", encoding="utf-8") as infile:
+            lint = lambda text: []  # TODO: Функция синтаксического анализа,
+            # принимает текст файла, возвращает замечания и ошибки
+            marks = lint(infile.read())
+            for mark in marks:
+                print(mark)
 
 
 if __name__ == '__main__':
-    main()
+    # parser = argparse.ArgumentParser()
+    if len(sys.argv) > 1:
+        main(*sys.argv[1:])
+    else:
+        main("test.sql")
