e72145e0c1aee3259983ce73043b41e925a8c3fe
Pavel Solomatin <crave_ozer_man@mail.ru>
2022-08-06 13:50:32 +0300

    feat: правила к запросу

---
+++ src/test.py
@@ -0,0 +1,17 @@
+import pathlib
+from src.main import main
+
+
+def f_test_all():
+    files = [
+        "sql/test.sql",
+        "sql/test_create.sql",
+        "sql/wrong_syntax.sql"
+    ]
+    for file in files:
+        line = pathlib.Path(file).read_text()
+        main(line)
+
+
+if __name__ == '__main__':
+    f_test_all()
